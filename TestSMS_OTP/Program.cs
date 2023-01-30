7e5ca0198e9eb0c2c62277f2d52d82b67dc1c993
Nodirbek-Abdulaxadov <nbkabdulaxadov@gmail.com>
2023-01-30 17:26:36 +0500

    SMS otp service added

---
+++ TestSMS_OTP/Program.cs
@@ -0,0 +1,29 @@
+﻿using Messager;
+
+Console.WriteLine("Enter your phone number:");
+string number = Console.ReadLine();
+
+Message message = new Message();
+var res = await message.SendSMSAsync(number);
+
+
+if (res.Success)
+{
+    a:
+    Console.WriteLine("Enter validation code:");
+    int code = int.Parse(Console.ReadLine());
+
+    if (code == res.Code)
+    {
+        Console.WriteLine("Success!");
+    }
+    else
+    {
+        Console.WriteLine("Wrong code. Try again!");
+        goto a;
+    }
+}
+else
+{
+    Console.WriteLine(res.Message);
+}
